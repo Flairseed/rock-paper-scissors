596024be9a2a3200b1f0436ba32bcf2b8e889c68
jay <rameshpvb@gmail.com>
2023-10-13 23:47:45 +0800

    feat: create function to get comp choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,10 @@
 const choices = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice () {
-  
+  // find a random index
+  numberOfChoices = choices.length;
+  choiceIndex = Math.floor(Math.random() * numberOfChoices);
+  // use the index to return the choice
+  choice = choices[choiceIndex];
+  return choice;
 }
