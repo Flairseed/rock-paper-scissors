5bd43b38ac2f6550a56481bb5b404295fc1dc99d
jay <rameshpvb@gmail.com>
2023-10-13 23:53:04 +0800

    refactor: add let to variables

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,9 +2,10 @@ const choices = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice () {
   // find a random index
-  numberOfChoices = choices.length;
-  choiceIndex = Math.floor(Math.random() * numberOfChoices);
+  let numberOfChoices = choices.length;
+  let choiceIndex = Math.floor(Math.random() * numberOfChoices);
   // use the index to return the choice
-  choice = choices[choiceIndex];
+  let choice = choices[choiceIndex];
   return choice;
 }
+
